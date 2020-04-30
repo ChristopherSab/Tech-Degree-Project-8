1043dce4c7d6b80db9c134c32d0f5a1dbafc9d84
ChristopherSab <chris.sabaoth@protonmail.com>
2020-04-30 10:40:12 +0100

    Replaced foreach to for loop for fetch url that creates new employee

--- js/main.js
+++ js/main.js
@@ -10,30 +10,19 @@ const url = 'https://randomuser.me/api/?results=12';
     fetch(url)
     .then((response) => response.json())
     .then(function(data) {
-        
-        data.results.forEach(person => {
 
-            p =  `<div class="image-box">
-            <img src="${person.picture.medium}" alt="">
+      for(let i=0; i < data.results.length; i++) {
+
+        employeeBox[i].innerHTML =   
+        
+            `<div class="image-box">
+                <img src="${data.results[i].picture.medium}" alt="">
             </div>
             <div class="employee-info">
-            <h2>${person.name.first} ${person.name.last}</h2>
-            <p>${person.email}</p>
-            <p>${person.location.city}</p>
+                <h2>${data.results[i].name.first} ${data.results[i].name.last}</h2>
+                <p>${data.results[i].email}</p>
+                <p>${data.results[i].location.city}</p>
             </div>`;
-
-            console.log(p);
-
-            for(let i =0; i < data.results.length; i++){
-                employeeBox[i].innerHTML = p;
-            }
-            
-
-
-        })
-        
+            console.log(data.results[i]);  
+      }  
     })
-
-
-
-    
